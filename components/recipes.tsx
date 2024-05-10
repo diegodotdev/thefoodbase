96f56ad7b0a56a23d7c574e34c77ad84705fbdb6
Diego <diegohxxiv@gmail.com>
2024-05-10 14:15:49 -0700

    run 6

--- components/recipes.tsx
+++ components/recipes.tsx
@@ -1,11 +1,10 @@
-import { Recipe } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Recipes({ data }: { data: Recipe[] }) {
+export default function Recipes({ data }: { data: any }) {
   return (
     <div className="w-full py-5 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-5 place-items-start">
-      {data?.map((i) => (
+      {data?.map((i: any) => (
         <Link href={`/recipes/${i.id}`} className="w-full" key={i?.id}>
           <div className="w-full flex flex-col gap-2">
             <div className="w-full h-[200px] relative overflow-hidden rounded-lg">
